c6fbe6dfea417a3b0fda2bf28062ce50e1db5c47
Enzo Dayrit <enzodayrit11@gmail.com>
2013-11-14 21:10:21 +0900

    added LearnUnity project

---
+++ LearnUnity/Assets/GridSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridSpawner : MonoBehaviour {
+	
+	public Transform gridCube;
+
+	
+	// Use this for initialization
+	void Start () {
+		int[] grid1 = 
+		{1, 0, 0, 0, 0,
+			0, 0, 0, 0, 0,
+			0, 0, 0, 0, 1,
+			0, 0, 0, 0, 0};
+
+		for(int i=0;i<20;++i){
+			
+			if(grid1[i] == 1){
+				Instantiate(gridCube, new Vector3(i%5,i/5,0), Quaternion.identity);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ LearnUnity/Assets/StartBounce.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartBounce : MonoBehaviour {
+	
+	public Transform bcube;
+	// Use this for initialization
+	void Start () {
+		for(int i=0;i<5;++i){
+			Instantiate(bcube, new Vector3(i,i,-3),  Quaternion.identity);
+			
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
