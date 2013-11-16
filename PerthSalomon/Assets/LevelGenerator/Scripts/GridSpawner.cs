4644ac3dc3b46566b711abcd685aa33add43b08d
thisisscatterbrain <arno.luebke@googlemail.com>
2013-11-16 17:23:32 +0900

    Added Player to the Level & Simple Player Controls

---
+++ PerthSalomon/Assets/LevelGenerator/Scripts/GridSpawner.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class GridSpawner : MonoBehaviour 
+{	
+	public GameObject gridCube;
+	public GameObject player;
+
+	// parse in thing
+	string[,] Parse() 
+	{
+		string[] values = File.ReadAllLines("Book1.csv");
+		int height = values.Length;
+		string[] t = values[0].Split(',');
+
+		string[,] array = new string[height,t.Length];
+
+		for(int i = 0; i < values.Length; ++i)
+		{
+			string[] st = values[i].Split(',');
+
+			for(int j = 0; j < st.Length; ++j)
+			{
+				array[i,j] = st[j];
+			}
+		}
+
+		return array;
+	}
+	
+	void Start() 
+	{
+		string[,] grid1 = this.Parse();
+
+		int width = grid1.GetLength(1);
+		int height = grid1.GetLength(0);
+
+		float xs = -3.5f;
+		float ys = 2.5f;
+
+		for (int i = 0; i < height; ++i)
+		{
+			for (int j = 0; j < width; ++j)
+			{
+				Debug.Log(grid1[i,j] + " ");
+
+				if (grid1[i,j] == "1")
+				{
+					Instantiate(gridCube, new Vector3(xs + j, ys - i, 0), Quaternion.identity);
+				}
+				else if (grid1[i,j] == "s")
+				{
+					Debug.Log("s here");
+					Instantiate(player, new Vector3(xs + j, ys - i, 0), Quaternion.identity);
+				}
+			}
+		}
+
+
+	}
+
+	void Update ()
+	{
+	
+	}
+}
---
+++ PerthSalomon/Assets/Player/Controls.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controls : MonoBehaviour 
+{
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+		Vector3 pos = this.transform.position;
+
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			pos.y += 1;
+			Debug.Log("Hello From Player");
+		}
+
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			pos.y -= 1;
+			Debug.Log("Hello From Player");
+		}
+
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			pos.x -= 1;
+			Debug.Log("Hello From Player");
+		}
+
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			pos.x += 1;
+			Debug.Log("Hello From Player");
+		}
+
+		this.transform.position = pos;
+	}
+}
