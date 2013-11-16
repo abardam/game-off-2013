13ce8ecff1c6890a7902a45619f325e60047bf3d
thisisscatterbrain <arno.luebke@googlemail.com>
2013-11-16 14:36:16 +0900

    Super Basic Menu Functionality

--- PerthSalomon/Assets/UserInterface/Scripts/Button.cs
+++ PerthSalomon/Assets/UserInterface/Scripts/Button.cs
@@ -1,15 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Button : MonoBehaviour {
+[RequireComponent (typeof(AudioSource))]
+public class Button : MonoBehaviour 
+{
+	public GUISkin skin;
 
-	// Use this for initialization
-	void Start () {
-	
+	// a delegate describing a click handler
+	public delegate void OnClick();
+
+	// a click handler that is called when set and the button is clicked.
+	public OnClick clickHandler;
+
+	public string text;
+	public Vector2 relPos;
+	private Vector2 screenPos;
+	public Vector2 screenDim;
+
+	private AudioSource clickSound;
+
+
+	void Start()
+	{
+		this.screenPos.x = this.relPos.x*Screen.width - this.screenDim.x/2;
+		this.screenPos.y = this.relPos.y*Screen.height - this.screenDim.y/2;
+		this.clickSound = this.GetComponent<AudioSource>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void OnGUI()
+	{
+		Rect r = new Rect(screenPos.x, screenPos.y, screenDim.x, screenDim.y);
+
+		if (this.skin != null)
+		{
+			GUI.skin = this.skin;
+		}
+		
+		if (GUI.Button(r, this.text) && null != this.clickHandler)
+		{
+			this.clickSound.Play();
+			this.clickHandler();
+		}
 	}
 }
---
+++ PerthSalomon/Assets/UserInterface/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour 
+{
+	public Button startButton;
+
+	private string hello = "Hi there!";
+
+	void Start () 
+	{
+		if (startButton != null)
+		{
+			Debug.Log("bla");
+			startButton.clickHandler = SayHello;
+		}
+	}
+
+	void Update ()
+	{
+	
+	}
+
+	private void SayHello()
+	{
+		Debug.Log(hello);
+	}
+}
