b7b16e575c6bc2a103eadfecbac34f18df224e52
thisisscatterbrain <arno.luebke@googlemail.com>
2013-11-23 15:03:47 +0900

    Exchanged renderer for player

---
+++ PerthSalomon/Assets/Player/Scripts/SingleAnimation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingleAnimation : MonoBehaviour 
+{
+	public Texture Atlas;
+	public GameObject Animatee;
+
+	void Start ()
+	{
+	
+	}
+
+	void Update () 
+	{
+	
+	}
+}
