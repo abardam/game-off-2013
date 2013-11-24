df372ed0f1fd70d7b66fff9ee510c6548da2e710
thisisscatterbrain <arno.luebke@googlemail.com>
2013-11-24 20:02:03 +0900

    first simple player animation

--- PerthSalomon/Assets/Player/Scripts/Controller/PlayerControllerStateDiving.cs
+++ PerthSalomon/Assets/Player/Scripts/Controller/PlayerControllerStateDiving.cs
@@ -3,10 +3,43 @@ using System.Collections;
 
 public class PlayerControllerStateDiving : PlayerControllerState
 {	
+	enum DiveState
+	{
+		DIVE_STATE_IDLE = 0,
+		DIVE_STATE_LEFT,
+		DIVE_STATE_RIGHT,
+	};
+
+	enum Event
+	{
+		EVENT_RIGHT_ARROW_DOWN = 0,
+		EVENT_LEFT_ARROW_DOWN,
+		EVENT_RIGHT_ARROW_UP,
+		EVENT_LEFT_ARROW_UP
+	};
+
+	DiveState[,] delta = {
+		{DiveState.DIVE_STATE_RIGHT, DiveState.DIVE_STATE_LEFT, DiveState.DIVE_STATE_LEFT, DiveState.DIVE_STATE_RIGHT}, // transitions for IDLE state
+		{DiveState.DIVE_STATE_IDLE, DiveState.DIVE_STATE_IDLE, DiveState.DIVE_STATE_IDLE, DiveState.DIVE_STATE_IDLE}, // transitions fo DIVE_STATE_LEFT
+		{DiveState.DIVE_STATE_IDLE, DiveState.DIVE_STATE_IDLE, DiveState.DIVE_STATE_IDLE, DiveState.DIVE_STATE_IDLE} // transitions fo DIVE_STATE_RIGHT
+	};
+
+
+	private DiveState state;
+
+	public PlayerControllerStateDiving()
+	{
+		this.state = DiveState.DIVE_STATE_IDLE;
+	}
+
 	public override void Update(PlayerController playerController) 
 	{
+		this.UpdateDiveState();
+
 		Vector3 v = Vector3.zero;
-		
+
+
+
 		if (Input.GetKey(KeyCode.LeftArrow))
 		{
 			v.x -= 1.0f;
@@ -32,5 +65,33 @@ public class PlayerControllerStateDiving : PlayerControllerState
 		v *= (playerController.speed*Time.deltaTime);
 		
 		playerController.GetCharacterController().Move(v);
+	}
+
+	private void UpdateDiveState()
+	{
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			this.state = delta[(int)this.state, (int)Event.EVENT_RIGHT_ARROW_DOWN];
+			Debug.Log(this.state);
+		}
+		
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			this.state = delta[(int)this.state, (int)Event.EVENT_LEFT_ARROW_DOWN];
+			Debug.Log(this.state);
+		}
+
+		if (Input.GetKeyUp(KeyCode.RightArrow))
+		{
+			this.state = delta[(int)this.state, (int)Event.EVENT_RIGHT_ARROW_UP];
+			Debug.Log(this.state);
+		}
+		
+		if (Input.GetKeyUp(KeyCode.LeftArrow))
+		{
+			this.state = delta[(int)this.state, (int)Event.EVENT_LEFT_ARROW_UP];
+			Debug.Log(this.state);
+		}
+
 	}
 }
