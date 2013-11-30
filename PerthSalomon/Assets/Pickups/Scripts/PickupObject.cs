720781c9f9c5e81e75d59b676633fdfc5efccdae
Enzo Dayrit <enzodayrit11@gmail.com>
2013-12-01 04:57:42 +0900

    stuff

---
+++ PerthSalomon/Assets/Pickups/Scripts/PickupObject.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupObject : MonoBehaviour {
+
+	public enum PickupType{
+		Coin, Key
+	};
+
+	public PickupType pickupType;
+	private int id;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider c){
+		if(c.gameObject.GetComponent<PlayerController>() != null){
+			switch(pickupType){
+			case PickupType.Coin:
+				break;
+			case PickupType.Key:
+				break;
+			}
+
+		}
+	}
+
+	public int ID {
+		get {
+			return id;
+		}
+		set {
+			id = value;
+		}
+	}
+}
